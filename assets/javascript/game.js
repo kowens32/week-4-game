bd28ae5a15f739c94dcf8ce57b7af9e1635363ee
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-27 21:58:18 -0400

    intial push

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//User has to click on an image to select the specific character to play
+
+//Remaining characters will move to a new section on the screen called "Opponent Arena"
+
+//User has to click on an image to select the specific character to challenge as an opponent
+
+//The user selected enemy will move to the "Defender Arena"
+
+//
