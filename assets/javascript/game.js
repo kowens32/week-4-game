fd70161a680a27d2944c9f809afe16e573713ff1
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-08-29 12:13:19 -0400

    midday push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,11 @@
-
-var obiHP = 100
-var lukeHP = 90
-var maulHP = 120
-var sidiousHP = 140
-
 //make the characters an object w/ HP as properties
+var obi = {healthPoints:"125" };
+var luke = {healthPoints:"120" };
+var maul = {healthPoints:"140" };
+var sidious = {healthPoints:"150" };
 
-//User has to click on an image to select the specific character to play
+        //User has to click on an image to select the specific character to play
 
-//User selected player will move to "Challenger Arena"
-// Move Buttons (Keyboard Down)
 $(document).ready(function() {
     $(".players-container .img-responsive").on("click", function() {
         // create variable to hold img src.
@@ -22,30 +18,25 @@ $(document).ready(function() {
         // append img to appropriate container
         $(".user-container").append(img);
         $(this).remove() && $(".enemy-container").append($( ".players-container" ) );
-        // $(".players-container").replaceWith(" ");
-        // $(".test").append(img);
-
-     //not sure i need this
-        //$(".players-container").addClass("remaining-players").removeClass("players-container");
-
+        //User has to click on an image to select the specific character to challenge as the enemy
+        $(".enemy-container .img-responsive").on("click", function() {
+            // create variable to hold img src.
+            var src = $(this).attr("src");
+            // create new img tag
+            var img = $('<img id="obi">');
+            // set src of img tag with variable above
+            img.attr('src', src);
+            img.attr("class", "img-responsive");
+            // append img to appropriate container
+            $(".test").append(img);
+        });
     });
-
-    $(".test .img-responsive").on("click", function() {
-        // create variable to hold img src.
-        var src = $(this).attr("src");
-        // create new img tag
-        var img = $('<img id="obi">');
-        // set src of img tag with variable above
-        img.attr('src', src);
-        img.attr("class", "img-responsive");
-        // append img to appropriate container
-        $(".enemy-container").append(img);
-        $(this).remove();
-
-    });
-
-
 });
+// $(".players-container").replaceWith(" ");
+// $(".test").append(img);
+
+//not sure i need this---changes class of containers
+//$(".players-container").addClass("remaining-players").removeClass("players-container");
 
 //Remaining characters will move to a new section on the screen called "Waiting Arena"
 
@@ -55,30 +46,26 @@ $(document).ready(function() {
 
 //User will click "Attack" in which the enemy will lose Health Points "HP"
 
-
+//Attack points must be randomly generated
 $(".attack-button").on("click", function () {
     // ... we generate a random number
     var random = Math.floor(Math.random() * 10) + 1;
     //HP has to be stored
     console.log("is this hp " + random);
     $(".attack-button").on("click", function () {
-        //The randomly generated HP will be stored for the next opponent and increased each time the "Attack button is pressed
-//etc 8, 16, 24, 32
+        //The randomly generated Attack points will be stored for the next opponent and increased each time the "Attack button is pressed
+        //etc 8, 16, 24, 32
         var increasedHP = (random + random)
         console.log("new HP " + increasedHP);
 
 
-    $(".attack-button").on("click", function () {
-        //The randomly generated HP will be stored for the next opponent and increased each time the "Attack button is pressed
-//etc 8, 16, 24, 32
-        var sol = maulHP - increasedHP
-        console.log("decreasing hp " + sol);
+        $(".attack-button").on("click", function () {
+            //The randomly generated HP will be stored for the next opponent and increased each time the "Attack button is pressed
+            //etc 8, 16, 24, 32
+            var sol = maulHP - increasedHP
+            console.log("decreasing hp " + sol);
+        });
     });
-
-    });
-
-
-
 });
 
 
